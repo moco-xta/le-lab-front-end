c58146611a8ae437676c923fd718075d3935faa2
moco-xta <moco.xta@gmail.com>
2025-04-08 22:06:38 +0200

    Hero: HeroHeading animation

--- src/components/layout/slices/home/hero/hero_heading/index.tsx
+++ src/components/layout/slices/home/hero/hero_heading/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { gsap } from 'gsap'
+import { useGSAP } from '@gsap/react'
 import { useTranslations } from 'next-intl'
 
 import './index.scss'
@@ -6,11 +8,34 @@ import './index.scss'
 export default function HeroHeading() {
   const t = useTranslations('HOME.HERO')
 
+  useGSAP(() => {
+    const animationFn = gsap.to('.hero-heading-line', {
+      yPercent: -100,
+      rotateZ: '0deg',
+      opacity: 1,
+      duration: 0.75,
+      delay: 0.5,
+      ease: 'power1.out',
+      stagger: 0.15,
+    })
+
+    return () => {
+      animationFn.kill()
+    }
+  })
+
   return (
-    <p id='hero-heading'>
-      <span>{t('TEXT.LINE_1')}</span>
-      <span>{t('TEXT.LINE_2')}</span>
-      <span>{t('TEXT.LINE_3')}</span>
-    </p>
+    <div id='hero-heading'>
+      {Array(3)
+        .fill(null)
+        .map((_, index) => (
+          <div
+            key={`heading_line_${index + 1}`}
+            className='hero-heading-line-wrapper'
+          >
+            <p className='hero-heading-line'>{t(`TEXT.LINE_${index + 1}`)}</p>
+          </div>
+        ))}
+    </div>
   )
 }
