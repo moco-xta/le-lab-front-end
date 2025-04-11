82625e096d9b2ab5f526d293008b94154c0178b1
moco-xta <moco.xta@gmail.com>
2025-04-11 09:59:45 +0200

    ReelCanvas: wrong solution (removed) -> NEW solution

--- src/components/layout/slices/home/introduction/index.tsx
+++ src/components/layout/slices/home/introduction/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import ParisMetroMap from './paris_metro_map'
 import Content from './content'
-import ReelCanvas from '@/components/three/canvas/reel_canvas/ReelCanvas'
 
 import './index.scss'
 
@@ -10,7 +9,6 @@ export default function Introduction() {
   return (
     <section id='introduction-section'>
       <Content />
-      <ReelCanvas />
       <ParisMetroMap />
     </section>
   )
